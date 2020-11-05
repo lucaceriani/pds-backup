369266c656b8e194de6db90e10685f6e1a9fa30a
Luca Ceriani <luca.ceri@gmail.com>
2020-11-05 15:47:37 +0100

    setup_env ok

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# download boost
+echo "Downloadgin Boost 1.74.0..."
+wget "https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz"
+
+echo "Extracting Boost..."
+tar -xf boost_1_74_0.tar.gz
+
+echo "Installing Boost..."
+cd boost_1_74_0
+./bootstrap.sh --prefix=$(pwd)/../boost
+./b2 install
 
